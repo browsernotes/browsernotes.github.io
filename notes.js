b82e5c2e5d756e6ad9b5e613198ef002a9c1e1fa
matthew-via-music <mvm@matthewviamusic.com>
2023-01-23 20:38:54 +0000

    refactor to use ids + add delete buttons with confirm option

--- notes.js
+++ notes.js
@@ -13,34 +13,33 @@ obj.logName() // functions are ignore by JSON (the delivery format for storage d
 //
 // Session storage - data stored for duration of web page being open
 
-/* sessionStorage.setItem("mySessionStoreObj", JSON.stringify(obj))
+sessionStorage.setItem("mySessionStoreObj", JSON.stringify(obj))
 sessionStorage.setItem("mySessionStoreArr", JSON.stringify(arr))
 const mySessionDataObj = JSON.parse(sessionStorage.getItem("mySessionStoreObj"))
 const mySessionDataArr = JSON.parse(sessionStorage.getItem("mySessionStoreArr"))
 console.log(mySessionDataObj)
 console.log(mySessionDataArr)
- */
 
 //
 // Local storage - persistent data storage remains after webpage is closed
 
-/* localStorage.setItem("myLocalStoreObj", JSON.stringify(obj))
+localStorage.setItem("myLocalStoreObj", JSON.stringify(obj))
 localStorage.setItem("myLocalStoreArr", JSON.stringify(arr))
 const myLocalDataObj = JSON.parse(localStorage.getItem("myLocalStoreObj"))
 const myLocalDataArr = JSON.parse(localStorage.getItem("myLocalStoreArr"))
 console.log(myLocalDataObj)
-console.log(myLocalDataArr) */
+console.log(myLocalDataArr)
 
 //
 // Remove or clear local storage data
 
-/* localStorage.setItem("foo", JSON.stringify(obj))
+localStorage.setItem("foo", JSON.stringify(obj))
 // Remove a single item or Clear multiple items of storage data:
 localStorage.removeItem("foo")
 // localStorage.clear()
 const bar = JSON.parse(localStorage.getItem("foo"))
 console.log(bar)
-*/
+
 
 // Local storage key
 
